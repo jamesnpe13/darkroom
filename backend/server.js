c6ea2ddd1f6b536f0c8ea055c44c7358468c7a5a
James Elazegui <jameselazegui21@gmail.com>
2023-03-15 11:09:27 +1300

    updated endpoints server

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,67 @@
 const express = require("express");
 const app = express();
-
 const mongoose = require("mongoose");
-
 require("dotenv").config();
-
 app.use(express.json());
 
 // models
-const User = require("./models/user");
+const Post = require("./models/post");
 
+const newPost = new Post({
+   title: "The Forest",
+   caption: "asdasdqsdasd",
+   date: new Date(),
+   likes: 5,
+});
+  
 // routes
 
-app.get("/", async (req, res) => {
-   const userArray = await User.find({});
-   res.json(userArray);
+// get all posts
+app.get("/posts", (req, res) => {
+   
 });
 
-// connection
+// get user's posts
+app.get("/posts/:user", (req, res) => {
+   //
+});
 
-mongoose.connect(process.env.DB_STRING).catch((error) => {
+// create a new post
+app.post("/posts", (req, res) => {
+   //
+});
+
+// update an existing post
+app.put("/posts/:id", (req, res) => {
+   //
+});
+
+// delete a post  
+app.delete("/posts/:id", (req, res) => {
+   // 
+});
+
+// app.post("/posts", async (req, res) => {
+//    await newPost.save();
+//    const postArray = await Post.find({});
+//    res.json(postArray);
+// });
+
+// app.get("/posts", async (req, res) => {
+//    const postArray = await Post.find({});
+//    res.json(postArray);
+// });
+
+// connection
+main().catch((error) => {
    console.log(error);
 });
 
+async function main() {
+   await mongoose.connect(process.env.DB_STRING);
+}
+
+// server listen
 app.listen(process.env.PORT, () => {
    console.log(`Listening in port: ${process.env.PORT}`);
 });
