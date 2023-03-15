869e00b5fd07166dfd7f8450cdf27f28e05519d1
James Elazegui <jameselazegui21@gmail.com>
2023-03-15 17:02:50 +1300

    Fixed navbar import. Added router location in header bar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,5 @@
 import "./App.scss";
-import {
-    BrowserRouter,
-    Route,
-    Router,
-    Routes,
-    useLocation,
-} from "react-router-dom";
+import { BrowserRouter, Route, Router, Routes, useLocation } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
 import Headerbar from "./components/Headerbar";
@@ -14,24 +8,25 @@ import Notfound from "./pages/Notfound";
 import Home from "./pages/Home";
 import Gallery from "./pages/Gallery";
 import { useState } from "react";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
-    //  const [currentPage, setCurrentPage] = useState(useLocation);
+   //  const [currentPage, setCurrentPage] = useState(useLocation);
 
-    return (
-        <div className="App">
+   return (
+      <div className="App">
+         <BrowserRouter>
             <Headerbar />
-
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/gallery" element={<Gallery />} />
-                    <Route path="*" element={<Notfound />} />
-                </Routes>
-                <NavBar />
-            </BrowserRouter>
-        </div>
-    );
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="/gallery" element={<Gallery />} />
+               <Route path="/createpost" element={<CreatePost />} />
+               <Route path="*" element={<Notfound />} />
+            </Routes>
+            <NavBar />
+         </BrowserRouter>
+      </div>
+   );
 }
 
 export default App;
---
+++ frontend/src/pages/CreatePost.js
@@ -0,0 +1,9 @@
+import "./CreatePost";
+
+export default function CreatePost() {
+   return (
+      <div className="create-post page">
+         <input type="text" />
+      </div>
+   );
+}
