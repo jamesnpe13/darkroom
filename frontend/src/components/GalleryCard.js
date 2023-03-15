e0ce6cb8d5eb4586e94ea9d2eed3dc92007f3338
elliesaxby <ellie.saxby1@gmail.com>
2023-03-16 09:02:34 +1300

    gallery card

---
+++ frontend/src/components/GalleryCard.js
@@ -0,0 +1,27 @@
+import "./GalleryCard.scss";
+
+const GalleryCard = () => {
+    return (
+        <div className="gallery page">
+            <h1 className="day">Today</h1>
+            <div className="gallery-card">
+                <div>
+                    <p className="card-title">Post Title </p>{" "}
+                    {/* api title goes here */}
+                    <p className="card-description">
+                        {/* api description goes here */}
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Incidunt maxime sequi atque.
+                    </p>
+                </div>
+                <div>
+                    <img src={"./images/placeholder-image.png"}></img>{" "}
+                    {/* api image goes here */}
+                </div>
+            </div>
+            <h1 className="day">Yesterday</h1>
+        </div>
+    );
+};
+
+export default GalleryCard;
