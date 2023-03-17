8659cd9e2a6c60fa66f66c0ceb66c653fe7718c0
James Elazegui <jameselazegui21@gmail.com>
2023-03-17 13:15:49 +1300

    fix gallery page and components

--- frontend/src/components/GalleryCard.js
+++ frontend/src/components/GalleryCard.js
@@ -1,25 +1,20 @@
 import "./GalleryCard.scss";
 
 const GalleryCard = () => {
-    return (
-        <div className="gallery page">
-            <div className="gallery-card">
-                <div>
-                    <p className="card-title">Post Title </p>{" "}
-                    {/* api title goes here */}
-                    <p className="card-description">
-                        {/* api description goes here */}
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Incidunt maxime sequi atque.
-                    </p>
-                </div>
-                <div>
-                    <img src={"./images/placeholder-image.png"} alt=""></img>{" "}
-                    {/* api image goes here */}
-                </div>
-            </div>
-        </div>
-    );
+   return (
+      <div className="gallery-card">
+         <div>
+            <p className="card-title">Post Title </p> {/* api title goes here */}
+            <p className="card-description">
+               {/* api description goes here */}
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt maxime sequi atque.
+            </p>
+         </div>
+         <div>
+            <img src={"./images/placeholder-image.png"} alt=""></img> {/* api image goes here */}
+         </div>
+      </div>
+   );
 };
 
 export default GalleryCard;
--- frontend/src/pages/Gallery.js
+++ frontend/src/pages/Gallery.js
@@ -3,10 +3,8 @@ import GalleryCard from "../components/GalleryCard";
 
 export default function Gallery() {
     return (
-        <div className="gallery page">
-            <h1 className="gallery-time">Today</h1>
+       <div className="gallery page">
             <GalleryCard />
-            <h1 className="gallery-time">Yesterday</h1>
         </div>
     );
 }
