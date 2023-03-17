c7b1eb4ad0490c4133adcaff918d452ebfc8b4a0
James Elazegui <jameselazegui21@gmail.com>
2023-03-17 13:31:58 +1300

    Merged main to branch

--- frontend/src/pages/Notfound.js
+++ frontend/src/pages/Notfound.js
@@ -1,7 +1,7 @@
 export default function Notfound() {
    return (
       <div className="not-found page">
-         <p>404 Page not Found</p>;
+         <p>404 Page not Found</p>
       </div>
    );
 }
