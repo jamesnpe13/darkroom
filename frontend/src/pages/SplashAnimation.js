ade7af43ebbf7827ff2957d116d91c1b6509de8b
James Elazegui <jameselazegui21@gmail.com>
2023-03-19 18:38:43 +1300

    Sign in form created. Logo to sign in transition added

--- frontend/src/pages/SplashAnimation.js
+++ frontend/src/pages/SplashAnimation.js
@@ -4,22 +4,25 @@ import { useState } from "react";
 
 export default function SplashAnimation() {
    const [logoIsVisible, setLogoIsVisible] = useState(true);
-   const [logoIsSmall, setLogoIsSmall] = useState(false);
+   const [formIsVisible, setFormIsVisible] = useState(false);
 
-   function removeLogo() {
-      //
+   function handleLoogoAnimEnd() {
+      setLogoIsVisible(false);
+      setFormIsVisible(true);
    }
 
    return (
       <div className="splash-animation">
-         <img
-            src={Logo}
-            onClick={() => {
-               setLogoIsSmall(true);
-            }}
-            style={{ display: `${logoIsVisible ? "block" : "none"}` }}
-            onAnimationEnd={removeLogo}
-         />
+         <img src={Logo} style={{ display: `${logoIsVisible ? "block" : "none"}` }} onAnimationEnd={handleLoogoAnimEnd} />
+
+         <form style={{ display: `${formIsVisible ? "flex" : "none"}` }}>
+            <h2>Log in to your account</h2>
+            <input type="text" className="username" placeholder="Username" />
+            <input type="password" className="password" placeholder="Password" />
+            <button type="submit" className="submit primary">
+               Sign in
+            </button>
+         </form>
       </div>
    );
 }
