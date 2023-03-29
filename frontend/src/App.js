43fedb5757ec85fb7f2cf5e39094cc29b9fcd447
James Elazegui <jameselazegui21@gmail.com>
2023-03-29 18:11:03 +1300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,6 +20,7 @@ function App() {
    useEffect(() => {}, [postsData]);
 
    function fetchAllPosts() {
+      // this is a test comment
       fetch("http://localhost:5000/posts/allposts", {
          headers: {
             "Content-Type": "application/json",
